42fa842264a6994a6831dc6e3f5b3a9b90be23dc
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2025-10-17 00:06:45 +0200

    add "Missing block model" and "Unable to bake model" for 1.18.2 Forge

--- 1.18.2/src/main/java/fr/samlegamer/addonslib/CustomLogFilter.java
+++ 1.18.2/src/main/java/fr/samlegamer/addonslib/CustomLogFilter.java
@@ -7,11 +7,13 @@ public class CustomLogFilter extends AbstractFilter {
     @Override
     public Result filter(LogEvent event) {
         if (event.getMessage().getFormattedMessage().contains("Exception loading blockstate definition")
+                || event.getMessage().getFormattedMessage().contains("Exception evaluating model definition")
                 || event.getMessage().getFormattedMessage().contains("Failed to load blockstate definition")
-        || event.getMessage().getFormattedMessage().contains("Missing block model")
+                || event.getMessage().getFormattedMessage().contains("Missing block model")
                 || event.getMessage().getFormattedMessage().contains("Missing model for variant")
                 || event.getMessage().getFormattedMessage().contains("Using missing texture, unable to load")
                 || event.getMessage().getFormattedMessage().contains("Unable to load model")
+                || event.getMessage().getFormattedMessage().contains("Unable to bake model")
                 || event.getMessage().getFormattedMessage().contains("Unable to resolve texture reference")){
             return Result.DENY;
         }
