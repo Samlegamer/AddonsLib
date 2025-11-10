f69358eb6c0cb9ba773c6e22e5a0e71f872c1561
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2025-11-10 19:08:06 +0100

    port 1.21.5 Forge to Furnitures 3.4.0

--- 1.21.5/src/main/java/fr/samlegamer/addonslib/AddonsLib.java
+++ 1.21.5/src/main/java/fr/samlegamer/addonslib/AddonsLib.java
@@ -1,7 +1,9 @@
 package fr.samlegamer.addonslib;
 
 import fr.samlegamer.addonslib.cfg.Cfg;
+import fr.samlegamer.addonslib.generation.loot_tables.LootModRegistry;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,25 +15,34 @@ import org.apache.logging.log4j.core.config.LoggerConfig;
 @Mod(AddonsLib.MODID)
 public class AddonsLib
 {
-	public static final String MODID = "addonslib";
-	public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MODID = "addonslib";
+    public static final Logger LOGGER = LogManager.getLogger();
 
-	public AddonsLib(FMLJavaModLoadingContext fmlJavaModLoadingContext)
-	{
-		fmlJavaModLoadingContext.registerConfig(ModConfig.Type.COMMON, Cfg.SPEC, "addonslib-common.toml");
-		LOGGER.info("AddonsLib Forge !");
-		if(Cfg.filterLogs.get())
-		{
-			addCustomFilter();
-		}
-	}
+    public AddonsLib(FMLJavaModLoadingContext fmlJavaModLoadingContext)
+    {
+        LootModRegistry.SERIALIZERS.register(fmlJavaModLoadingContext.getModEventBus());
+        fmlJavaModLoadingContext.registerConfig(ModConfig.Type.COMMON, Cfg.SPEC, "addonslib-common.toml");
+        LOGGER.info("AddonsLib Forge !");
+        fmlJavaModLoadingContext.getModEventBus().addListener(this::onConfigLoaded);
+    }
 
-	private void addCustomFilter() {
-		LoggerContext context = (LoggerContext) LogManager.getContext(false);
-		Configuration config = context.getConfiguration();
-		LoggerConfig loggerConfig = config.getLoggerConfig("net.minecraft.client");
+    private void onConfigLoaded(final ModConfigEvent.Loading event)
+    {
+        if (event.getConfig().getSpec() == Cfg.SPEC)
+        {
+            if (Cfg.filterLogs.get())
+            {
+                addCustomFilter();
+            }
+        }
+    }
 
-		loggerConfig.addFilter(new CustomLogFilter());
-		context.updateLoggers();
-	}
+    private void addCustomFilter() {
+        LoggerContext context = (LoggerContext) LogManager.getContext(false);
+        Configuration config = context.getConfiguration();
+        LoggerConfig loggerConfig = config.getLoggerConfig("net.minecraft.client");
+
+        loggerConfig.addFilter(new CustomLogFilter());
+        context.updateLoggers();
+    }
 }
